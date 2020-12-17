6c4b6c8ced160603a8175fee799c864736e177ab
Beto <ortega361@github.com>
2020-12-17 18:01:14 -0500

    added all dependencies for react component

--- MERN_skeleton/template.js
+++ MERN_skeleton/template.js
@@ -5,6 +5,8 @@ export default () => {
     <head> \
         <meta charset="utf-8"></meta> \
         <title>MERN Skeleton</title>  \
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400"> \
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> \
     </head> \
     <body>  \
         <div id="root">Hello world!!</div> \
