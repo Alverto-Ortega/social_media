b97786dfd1faf761b17c1ec33b401f8a3250e34d
Beto <ortega361@github.com>
2020-12-18 02:01:32 -0500

    added auth management helper methods ready for use in front end components

---
+++ MERN_skeleton/client/auth/auth-helper.js
@@ -0,0 +1,36 @@
+//helper methods to store and retrieve JWT credentials from client side 
+//using sessionStorage, also clearing out the sessionstorage on user-signout
+//localStorage can be used also but the user auth state will be rememebered across tabs in a browser 
+//sessionStorage remember only in current window tab
+
+const { SingleBedOutlined } = require("@material-ui/icons");
+
+authenticate(jwt, cb){
+    if(typeof window !== "undefined")
+        sessionStorage.setItem('jwt', JSON.stringify(jwt));
+    
+    cb();
+}
+
+isAuthenticated(){
+    if (typeof window == "undefined"){
+        return false;
+    }
+    if (sessionStorage.getItem('jwt')){
+        return JSON.parse(sessionStorage.getItem('jwt'));
+    }else{
+        return false;
+    }
+}
+
+//deleting credentials from storage when user signs out
+clearJWT(cb){
+    if(typeof window !== "undefined"){
+        sessionStorage.removeItem('jwt');
+    }
+    cb();
+    //optional for cookies used 
+    signout().then((data) => {
+        document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    });
+}
